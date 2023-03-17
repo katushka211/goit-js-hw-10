8f0c35a35e5eb155a7c2f9062ee9d9bc15dc07a9
Kateryna Verveda <katushka211@gmail.com>
2023-03-17 10:03:45 +0100

    add elements

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('click', onInputClick);
